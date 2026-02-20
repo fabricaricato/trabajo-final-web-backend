bd0430e6bb3cda3a0979e377f4d682afb0c0f55f
Fabrizio <fabriziocaricato1@gmail.com>
2026-02-20 00:04:45 -0300

    index.ts adapted for deployment on Vercel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import cors from "cors"
 import { config } from "dotenv"
 import { connectDb } from "./config/database"
@@ -6,6 +6,7 @@ import { bookRouter } from "./routes/bookRouter"
 import { authRouter } from "./routes/authRouter"
 import { validateToken } from "./middleware/authMiddleware"
 config()
+connectDb()
 
 const PORT = process.env.PORT
 
@@ -14,17 +15,33 @@ const server = express()
 server.use(cors())
 server.use(express.json())
 
+server.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <h1>RESTful API Running! 🚀</h1>
+    <p>Welcome to the Book Management API.</p>
+    <p>Main Endpoints:</p>
+    <ul>
+      <li>POST /api/auth/register (Register user)</li>
+      <li>POST /api/auth/login (Login / Get Token)</li>
+      <li>GET /api/books (View books - Token Required)</li>
+    </ul>
+    <h4>Please check the project's README.md file to get started!</h4>
+    <p>Developed by Fabrizio Caricato</p>
+  `)
+})
+
 server.use('/api/books', validateToken, bookRouter)
 server.use('/api/auth', authRouter)
 
-server.listen(PORT, () => {
-  try {
-    connectDb()
-    console.log(`Server listening on port: ${PORT}`)
-  } catch (error) {
-    const err = error as Error
-    console.log(`Port listening failure, error message: ${err.message}`)
-  }
-})
+if (!process.env.VERCEL) {
+  server.listen(PORT, () => {
+    try {
+      console.log(`Server listening on port: ${PORT}`)
+    } catch (error) {
+      const err = error as Error
+      console.log(`Port listening failure, error message: ${err.message}`)
+    }
+  })
+}
 
 export default server
